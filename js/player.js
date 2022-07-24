9544054255c6402eec7fc69634eed8d9984e6657
dahyukc <90508781+devdahyuk@users.noreply.github.com>
2022-07-24 15:20:07 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -6,21 +6,40 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
 function onYouTubeIframeAPIReady() {
-  var player;
-  player = new YT.Player('player', {
-    videoId: 'tB0sBzM_15E',
-    playerVars: { 'autoplay': 1, 'loop': 1, 'controls': 0 },
-    events: {
-      'onReady': onPlayerReady,
-      'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
-      'onStateChange': onPlayerStateChange,
-      'onError': onPlayerError
-    }
-  });
+    player = new YT.Player('player', {
+        height: '405',
+        width: '720',
+        videoId: 'tB0sBzM_15E',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,
+            'showinfo': 1,
+            'enablejsapi': 1,
+            'loop': 1,
+            'autohide': 1,
+            'playsinline': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
 }
 
 function onPlayerReady(event) {
-  event.target.setVolume(100);
-  event.target.playVideo();
+    event.target.playVideo();
+    if (player.getPlayerState() != 1) {
+        event.target.mute();
+        event.target.playVideo();
+    }
 }
